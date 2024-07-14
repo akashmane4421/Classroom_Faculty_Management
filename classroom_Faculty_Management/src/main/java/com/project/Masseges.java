5b034d87bee2a41fb219e93f3006a9d3fa4cb0bc
ADMIN <ADMIN@AKASH>
2024-07-14 19:51:22 +0530

    added messages

---
+++ classroom_Faculty_Management/src/main/java/com/project/Masseges.java
@@ -0,0 +1,38 @@
+package com.project;
+
+public class Masseges {
+
+	public static String insertData() {
+		return "Data insert Successfully...!";
+	}
+
+	public static String notInsertData() {
+		return "Data Not insert Successfully...!";
+		
+	}
+
+	public static String updateData() {
+		return "Data Update Successfully...!";
+	}
+
+	public static String notUpdateData() {
+		return "Data Not update Successfully...!";
+	}
+
+	public static String deleteData() {
+		return "Data Delete Successfully...!";
+	}
+
+	public static String notDeleteData() {
+		return "Data Not Delete Successfully...!";
+	}
+
+	public static String notFoundData() {
+		return "Data Is Not Found ...!";
+	}
+
+	public static String error() {
+		return "Something Went Wrong ...!";
+	}
+
+}
